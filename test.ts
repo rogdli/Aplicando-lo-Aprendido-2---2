1eb7d8226feb0b8ef1e9e6dcc269c38aa809c388
Rodrigo Villegas <128648211+rogdli@users.noreply.github.com>
2023-11-16 20:16:32 -0300

    Update test.ts

--- test.ts
+++ test.ts
@@ -5,30 +5,24 @@ interface Task {
   description: string;
   status: string;
   creationDate: string;
-  expireDate: string | null;
-  difficulty: string; 
+  lastEditDate: string;
+  expireDate: string;
+  difficulty: string;
 }
 
 const taskList: Task[] = [];
 
-function createTask(title: string, description: string = '', difficulty?: string, expireDate?: string): void {
+function createTask(title: string, description: string = '', difficulty: string, expireDate: string): void {
   const task: Task = {
     title,
     description,
     status: "pendiente",
     creationDate: new Date().toString(),
+    lastEditDate: new Date().toString(),
     expireDate,
     difficulty,
   };
   taskList.push(task);
-}
-
-function changeStatus(title: string, newStatus: string): void {
-  for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].title === title) {
-      taskList[i].status = newStatus;
-    }
-  }
 }
 
 function showTask(task: Task): void {
@@ -37,9 +31,20 @@ function showTask(task: Task): void {
   console.log("Descripción:", task.description);
   console.log("Fecha de creación:", task.creationDate);
   console.log("Fecha de vencimiento:", task.expireDate);
-  console.log("Dificultad:", task.difficulty);
+  console.log("Dificultad:", getDifficultyStars(task.difficulty));
+  console.log("Última edición:", task.lastEditDate);
+}
 
-  const response: string = readlineSync.question("\nPresione 'E' para editar la tarea, '0' para volver: ");
+function showTaskAndEdit(task: Task): void {
+  console.log("Titulo:", task.title);
+  console.log("Estado:", task.status);
+  console.log("Descripción:", task.description);
+  console.log("Fecha de creación:", task.creationDate);
+  console.log("Fecha de vencimiento:", task.expireDate);
+  console.log("Dificultad:", getDifficultyStars(task.difficulty));
+  console.log("Última edición:", task.lastEditDate);
+
+  const response = readlineSync.question("\nPresione 'E' para editar la tarea, '0' para volver: \n");
   if (response.toLowerCase() === 'e') {
     editTask(task);
   } else if (response === '0') {
@@ -50,16 +55,32 @@ function showTask(task: Task): void {
   }
 }
 
+function getDifficultyStars(difficulty: string): string {
+  switch (difficulty.toLowerCase()) {
+    case 'baja':
+      return '★☆☆';
+    case 'media':
+      return '★★☆';
+    case 'alta':
+      return '★★★';
+    default:
+      return difficulty;
+  }
+}
+
 function editTask(task: Task): void {
-  const newTitle: string = readlineSync.question("Nuevo título (Enter para mantener el actual): ") || task.title;
-  const newDescription: string = readlineSync.question("Nueva descripción (Enter para mantener la actual): ") || task.description;
-  const newDifficulty: string = readlineSync.question("Nueva dificultad (Enter para mantener la actual): ") || task.difficulty || '';
-  const newExpireDate: string = readlineSync.question("Nueva fecha de vencimiento (formato dd/mm/yyyy, Enter para mantener la actual): ") || task.expireDate || '';
+  const newTitle = readlineSync.question("Nuevo título (Enter para mantener el actual): ") || task.title;
+  const newDescription = readlineSync.question("Nueva descripción (Enter para mantener la actual): ") || task.description;
+  const newStatus = readlineSync.question("Nuevo estado (en curso/pendiente/terminada) (Enter para mantener el actual): ") || task.status;
+  const newDifficulty = readlineSync.question("Nueva dificultad (Enter para mantener la actual): ") || task.difficulty;
+  const newExpireDate = readlineSync.question("Nueva fecha de vencimiento (formato dd/mm/yyyy, Enter para mantener la actual): ") || task.expireDate;
 
   task.title = newTitle;
   task.description = newDescription;
+  task.status = newStatus;
   task.difficulty = newDifficulty;
   task.expireDate = newExpireDate;
+  task.lastEditDate = new Date().toString();
 
   console.log("Tarea editada con éxito.");
   readlineSync.question("Presiona Enter para volver al Menú Principal.");
@@ -81,45 +102,42 @@ function mainMenu(): void {
 
   displayMainMenu();
 
-  const mainMenuChoice: string = readlineSync.question("?");
+  const mainMenuOption = readlineSync.question("?");
 
-  switch (mainMenuChoice) {
+  switch (mainMenuOption) {
     case '1':
       displaySubMenu();
 
-      const subMenuChoice: string = readlineSync.question("?");
-
-      switch (subMenuChoice) {
+      const subMenuOption = readlineSync.question("?");
+      switch (subMenuOption) {
         case '1': // Todas
           console.log("Mostrando todas las tareas...\n");
-          for (let i = 0; i < taskList.length; i++) {
-            showTask(taskList[i]);
-          }
-          break;
 
-        case '2': // Pendientes
-          console.log("Mostrando todas las tareas pendientes...\n");
-          for (let i = 0; i < taskList.length; i++) {
-            if (taskList[i].status.toLowerCase() === "pendiente") {
-              showTask(taskList[i]);
-            }
+          taskList.sort((a, b) => a.title.localeCompare(b.title));
+
+          for (const task of taskList) {
+            showTask(task);
           }
+
+          readlineSync.question("Presiona Enter para volver al Menú Principal.");
           break;
 
         case '3': // En curso
           console.log("Mostrando todas las tareas en curso...\n");
-          for (let i = 0; i < taskList.length; i++) {
-            if (taskList[i].status.toLowerCase() === "en curso") {
-              showTask(taskList[i]);
+
+          for (const task of taskList) {
+            if (task.status.toLowerCase() === "en curso") {
+              showTask(task);
             }
           }
           break;
 
         case '4': // Terminadas
           console.log("Mostrando todas las tareas terminadas...\n");
-          for (let i = 0; i < taskList.length; i++) {
-            if (taskList[i].status.toLowerCase() === "terminadas") {
-              showTask(taskList[i]);
+
+          for (const task of taskList) {
+            if (task.status.toLowerCase() === "terminada") {
+              showTask(task);
             }
           }
           break;
@@ -128,32 +146,31 @@ function mainMenu(): void {
           console.log("Respuesta inválida. Vuelva a intentarlo.\n");
           break;
       }
-
       readlineSync.question("Presiona Enter para volver al Menú Principal.");
-      displayMainMenu();
       break;
 
     case '2':
-      const taskTitle: string = readlineSync.question("Ingrese el título de la tarea a buscar: ");
-      for (let i = 0; i < taskList.length; i++) {
-        if (taskList[i].title === taskTitle) {
-          showTask(taskList[i]);
-          break;
-        }
+      const taskTitle = readlineSync.question("Ingrese el título de la tarea a buscar: ");
+      const taskFound = taskList.find(task => task.title === taskTitle);
+
+      if (taskFound) {
+        showTaskAndEdit(taskFound);
+      } else {
+        console.log("Tarea no encontrada.");
       }
+
       readlineSync.question("Presiona Enter para volver al Menú Principal.");
-      mainMenu();
       break;
 
     case '3':
-      const title: string = readlineSync.question("Ingrese el título de la tarea: ");
-      const description: string = readlineSync.question("Ingrese la descripción de la tarea: ");
-      const difficulty: string = readlineSync.question("Ingrese la dificultad de la tarea: ");
-      const expireDate: string = readlineSync.question("Ingrese la fecha de vencimiento (formato dd/mm/yyyy): ");
+      const title = readlineSync.question("Ingrese el título de la tarea: ");
+      const description = readlineSync.question("Ingrese la descripción de la tarea: ");
+      const difficulty = readlineSync.question("Ingrese la dificultad de la tarea (baja/media/alta): ");
+      const expireDate = readlineSync.question("Ingrese la fecha de vencimiento (formato dd/mm/yyyy): ");
+
       createTask(title, description, difficulty, expireDate);
       console.log("Tarea creada con éxito.");
       readlineSync.question("Presiona Enter para volver al Menú Principal.");
-      mainMenu();
       break;
 
     case '0':
